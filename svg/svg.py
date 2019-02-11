88fe8f5a370ff9913054a64b5f91144d8f6a26c9
Tamas Nepusz <ntamas@gmail.com>
2019-02-11 14:16:42 +0100

    current point should be the start point of the path after a closepath command

--- svg/svg.py
+++ svg/svg.py
@@ -370,6 +370,7 @@ class Path(Transformable):
             # Close Path
                 l = Segment(current_pt, start_pt)
                 self.items.append(l)
+                current_pt = start_pt
 
 
             elif command in 'LHV':
@@ -677,4 +678,3 @@ for name, cls in inspect.getmembers(sys.modules[__name__], inspect.isclass):
     tag = getattr(cls, 'tag', None)
     if tag:
         svgClass[svg_ns + tag] = cls
-
